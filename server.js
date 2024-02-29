acf3cf4bef41efc50def5c6102ad819f64808d5a
Andrii Yurov <andriiyurov.dev@gmail.com>
2024-02-29 17:13:42 -0600

    cors

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ mongoose
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-type"],
+  })
+);
 
 fs.readdirSync("./routes").map((r) =>
   app.use("/api", require(`./routes/${r}`))
