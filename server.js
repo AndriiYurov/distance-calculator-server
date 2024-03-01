6ec49b4352d3356bd87b80edf569df2df795c678
Andrii Yurov <andriiyurov.dev@gmail.com>
2024-02-29 18:44:29 -0600

    cors

--- server.js
+++ server.js
@@ -12,13 +12,9 @@ mongoose
   .then(() => console.log("DB connected"))
   .catch((err) => console.log("DB connection error =>", err));
 
+app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: [process.env.CLIENT_URL],
-  })
-);
 
 fs.readdirSync("./routes").map((r) =>
   app.use("/api", require(`./routes/${r}`))
